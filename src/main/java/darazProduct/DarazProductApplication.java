9b3b96cb1062a2300b1b90b6a5387d82f53749da
rezwanul91 <mohammadhaquer@gmail.com>
2024-09-12 11:40:14 -0400

    create a repository

---
+++ src/main/java/darazProduct/DarazProductApplication.java
@@ -0,0 +1,13 @@
+package darazProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarazProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarazProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/darazProduct/DarazProductApplicationTests.java
@@ -0,0 +1,13 @@
+package darazProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarazProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
